fb09414668c0ebd73bc4d901984eeb1e862d5c8c
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-06-02 17:20:36 +0200

    premiere amelioration de la partie evolutions

--- compile.sh
+++ compile.sh
@@ -8,4 +8,9 @@ if [ ! -f "Images/DiagrammeDeSequence.png" ]; then
 	inkscape --export-png=Images/DiagrammeDeSequence.png Images/DiagrammeDeSequence.svg
 fi
 
+if [ ! -f "Images/illustrationProblemeIA.png" ]; then
+  wget http://i.imgur.com/64C1FmT.png -O Images/illustrationProblemeIA.png
+fi
+
+pdflatex -output-directory Rapport Rapport/rapport.tex
 pdflatex -output-directory Rapport Rapport/rapport.tex
