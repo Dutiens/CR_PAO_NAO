fb444a54d9c6d19bc09c5e3d695b155541a26806
Thibault THEOLOGIEN <thibault.theologien@insa-rouen.fr>
2016-06-11 19:41:29 +0200

    maj script de compilation -> ajout des images de reco à dl

--- compile.sh
+++ compile.sh
@@ -12,5 +12,17 @@ if [ ! -f "Images/illustrationProblemeIA.png" ]; then
   wget http://i.imgur.com/64C1FmT.png -O Images/illustrationProblemeIA.png
 fi
 
+if [ ! -f "Images/reco1.jpg" ]; then
+  wget http://i.imgur.com/g17tLtR.jpg -O Images/reco1.jpg
+fi
+
+if [ ! -f "Images/reco2.jpg" ]; then
+  wget http://i.imgur.com/aX9fgmF.jpg -O Images/reco2.jpg
+fi
+
+if [ ! -f "Images/reco3.jpg" ]; then
+  wget http://i.imgur.com/Uit3zA8.jpg -O Images/reco3.jpg
+fi
+
 pdflatex -output-directory Rapport Rapport/rapport.tex
 pdflatex -output-directory Rapport Rapport/rapport.tex
